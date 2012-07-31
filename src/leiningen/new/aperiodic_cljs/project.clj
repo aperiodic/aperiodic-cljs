5bb83e1c68b71807260c0f2cce220762dca4b03d
Dan Lidral-Porter <dlp@aperiodic.org>
2012-07-31 00:05:36 -0700

    Rectify licenses

--- src/leiningen/new/aperiodic_cljs/project.clj
+++ src/leiningen/new/aperiodic_cljs/project.clj
@@ -1,8 +1,8 @@
 (defproject {{raw-name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "GNU Lesser General Public License"
-            :url "http://www.gnu.org/licenses/lgpl.html"}
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.0.4"]
                  [hiccup "1.0.0"]]
